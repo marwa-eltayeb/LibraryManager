429b3e0f9ade66325ef5d3dd8d8b735c6d16d488
Marwa-Said <marwa_eltayeb@yahoo.com>
2021-05-04 18:39:45 +0200

    Create simple design

--- src/com/marwaeltayeb/lms/MainController.java
+++ src/com/marwaeltayeb/lms/MainController.java
@@ -1,6 +1,34 @@
 package com.marwaeltayeb.lms;
 
-public class MainController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class MainController implements Initializable {
+
+    TextField editId;
+    TextField editTitle;
+    TextField editAuthor;
+    TextField editYear;
+    TextField editPages;
+    Button btnInsert;
+    Button btnUpdate;
+    Button btnDelete;
+    TableView tvBooks;
+    TableColumn colISBN;
+    TableColumn colTitle;
+    TableColumn colAuthor;
+    TableColumn colYear;
+    TableColumn colPages;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
