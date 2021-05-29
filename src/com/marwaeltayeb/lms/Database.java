523abcee93192371798b93debb61a6c6e6cc244e
Marwa-Said <marwa_eltayeb@yahoo.com>
2021-05-29 16:37:58 +0200

    Get books from SQLite Database

--- src/com/marwaeltayeb/lms/Database.java
+++ src/com/marwaeltayeb/lms/Database.java
@@ -1,5 +1,8 @@
 package com.marwaeltayeb.lms;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import java.sql.*;
 
 public class Database {
@@ -15,5 +18,35 @@ public class Database {
             System.exit(0);
         }
         return connection;
+    }
+
+    public static ObservableList<Book> getBooksFromDB(){
+        ObservableList<Book> books = FXCollections.observableArrayList();
+        Connection connection = getConnection();
+        Statement stmt = null;
+        try {
+            connection.setAutoCommit(false);
+            stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM books;" );
+            while ( rs.next() ) {
+                int id = rs.getInt("id");
+                int isbn = rs.getInt("isbn");
+                String  title = rs.getString("title");
+                String  author = rs.getString("author");
+                int year = rs.getInt("year");
+                int pages = rs.getInt("pages");
+
+                Book book = new Book(id, isbn, title, author, year, pages);
+                books.add(book);
+            }
+            rs.close();
+            stmt.close();
+            connection.close();
+            System.out.println("Books Selected successfully");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        return books;
     }
 }
