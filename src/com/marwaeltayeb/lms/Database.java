c9eeea75c0de338df0d816a31153f25183a17557
Marwa-Said <marwa_eltayeb@yahoo.com>
2021-07-06 01:28:02 +0200

    Create executable jar

--- src/com/marwaeltayeb/lms/Database.java
+++ src/com/marwaeltayeb/lms/Database.java
@@ -11,7 +11,7 @@ public class Database {
         Connection connection = null;
         try {
             Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:db/books_db.db");
+            connection = DriverManager.getConnection("jdbc:sqlite::resource:books.db");
             System.out.println("Opened database successfully");
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
